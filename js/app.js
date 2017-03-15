45dbb5407eb89a764f255321f16ebc3201a6fd12
cgnzlz22 <cgnzlz22@yahoo.com>
2017-03-15 01:45:17 -0700

    FML I lost my randomize, and Ive gone color blind

--- js/app.js
+++ js/app.js
@@ -1,4 +1,63 @@
 /* setup your angular app here */
+var app = angular.module('fruitVeg', []);
+
+app.controller('mainCtrl', ['$scope', function($scope) {
+  $scope.fruits = [];
+  $scope.veggies = [];
+  
+ 	$scope.randomize = function(array) {
+    array.forEach(function(item, index){
+      array.splice(Math.floor(Math.random()*array.length), 0, array.splice(index, 1)[0]);
+    });
+    return array;
+  };
+	
+	$scope.Combo = fruits.concat(vegetables);
+
+  $scope.moveLeft = function(index) {
+    $scope.fruits.push($scope.Combo.splice(index, 1)[0]);
+  }
+
+  $scope.moveRight = function(index) {
+    $scope.veggies.push($scope.Combo.splice(index, 1)[0]);
+  }
+
+  $scope.moveLeft2Combo = function(index) {
+    $scope.Combo.push($scope.veggies.splice(index, 1)[0]);
+  }
+
+  $scope.moveRight2Combo = function(index) {
+    $scope.Combo.push($scope.fruits.splice(index, 1)[0]);
+  }
+  $scope.findVeggie = function(index) {
+    if(vegetables.indexOf($scope.veggies[index])>=0){
+      return true;
+    } else{
+      return false;
+    }
+  };
+  $scope.findFruit = function(index) {
+    if(fruits.indexOf($scope.fruits[index])>=0){
+      return true;
+    } else{
+      return false;
+    }
+  };
+  $scope.checkResult = function() {
+    var result = true;
+    $scope.fruits.forEach(function(fruit){
+      if(vegetables.indexOf(fruit)>=0){
+        result = false;
+      }
+    });
+    $scope.veggies.forEach(function(vegetable){
+      if(fruits.indexOf(vegetable)>=0){
+        result = false;
+      }
+    });
+    return result;
+  };
+}]);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
